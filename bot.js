39f7a026860fca46c7fe95f37548fd2744dc3e4c
tuagkcuf <jokersms222@gmail.com>
2023-05-28 11:35:54 +0300

    add imports

---
+++ bot.js
@@ -0,0 +1,17 @@
+// setup ethers, required variables, contracts and start function
+import { Wallet, ethers } from "ethers"
+import { FlashbotsBundleProvider, FlashbotsBundleProvider } from "@flashbots/ethers-provider-bundle"
+
+import {
+    UniswapAbi,
+    UniswapBytecode,
+    UniswapFactoryAbi,
+    UniswapFactoryBytecode,
+    pairAbi,
+    pairBytecode,
+    erc20Abi,
+    erc20Bytecode,
+    UniswapV3Abi,
+} from "./notes"
+
+
