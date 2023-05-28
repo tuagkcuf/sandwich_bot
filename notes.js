d466a795b08a9fe453f5e3bc93c67ab0e3090c54
tuagkcuf <jokersms222@gmail.com>
2023-05-28 11:54:50 +0300

    add addresses

--- notes.js
+++ notes.js
@@ -2058,6 +2058,11 @@ const UniswapV3Abi = [
     { stateMutability: "payable", type: "receive" },
 ]
 
+const wethAddressMainnet = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+const uniswapV2RouterAddressMainnet = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+const uniswapFactoryAddressMainnet = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+const universalRouterAddressMainnet = "0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B"
+
 export {
     UniswapAbi,
     UniswapBytecode,
